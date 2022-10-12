b1704576aba8e7e88ae44c3f1d90ba28a2594d6b
YaroslavGomon <pozzzitivu@mail.ru>
2022-10-13 00:57:38 +0400

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,33 +3,123 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  constructor(direction) {
+    this.direction = direction;
+  }
+
+  encrypt(message, key) {
+    if (arguments[0] && arguments[1]) {
+      let messageUppercase = message
+        .split(' ')
+        .map(val => val.toUpperCase())
+        .join('');
+      let keyFull = '';
+      let res = [];
+
+      while (keyFull.length !== messageUppercase.length) {
+        keyFull +=
+          key[
+            keyFull.length >= key.length
+              ? keyFull.length % key.length
+              : keyFull.length
+          ].toUpperCase();
+      }
+
+      for (let i = 0; i < keyFull.length; i++) {
+        let letter;
+        if (
+          messageUppercase[i].charCodeAt(0) >= 65 &&
+          messageUppercase[i].charCodeAt(0) <= 90
+        ) {
+          letter =
+            (messageUppercase[i].charCodeAt(0) + keyFull[i].charCodeAt(0)) % 26;
+          letter += 'A'.charCodeAt(0);
+        } else {
+          letter = messageUppercase[i].charCodeAt(0);
+        }
+
+        res.push(String.fromCharCode(letter));
+      }
+
+      for (let i = 0; i < message.length; i++) {
+        if (message[i] === ' ') {
+          res.splice(i, 0, ' ');
+        }
+      }
+
+      return (this.direction || this.direction === undefined) ? res.join('') : res.reverse().join('');
+    } else {
+      throw new Error('Incorrect arguments!');
+    }
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(message, key) {
+    if (arguments[0] && arguments[1]) {
+      let messageUppercase = message
+        .split(' ')
+        .map(val => val.toUpperCase())
+        .join('');
+      let keyFull = '';
+      let res = [];
+
+      while (keyFull.length !== messageUppercase.length) {
+        keyFull +=
+          key[
+            keyFull.length >= key.length
+              ? keyFull.length % key.length
+              : keyFull.length
+          ].toUpperCase();
+      }
+
+      for (let i = 0; i < keyFull.length; i++) {
+        let letter;
+        if (
+          messageUppercase[i].charCodeAt(0) >= 65 &&
+          messageUppercase[i].charCodeAt(0) <= 90
+        ) {
+          letter =
+            (messageUppercase[i].charCodeAt(0) - keyFull[i].charCodeAt(0) + 26) % 26;
+          letter += 'A'.charCodeAt(0);
+        } else {
+          letter = messageUppercase[i].charCodeAt(0);
+        }
+
+        res.push(String.fromCharCode(letter));
+      }
+
+      for (let i = 0; i < message.length; i++) {
+        if (message[i] === ' ') {
+          res.splice(i, 0, ' ');
+        }
+      }
+
+      return (this.direction || this.direction === undefined) ? res.join('') : res.reverse().join('');
+    } else {
+      throw new Error('Incorrect arguments!');
+    }
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
